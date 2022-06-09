78ad43e88cda56fad01c3678cb660fe4f970301b
Kyle <ktvitale7@gmail.com>
2022-06-09 19:56:36 -0400

    commit tree 98521d27000c2d4d22d6f301d02cd24c21d03fc5

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+// Class for cat
+class Cat {
+    constructor(name, sex){
+        this.name = name 
+        this.sex = sex
+        
+    }
+}
+let newCat = new Cat("Sasha", "female")
+newcCat.speak(`${this.name} says meow!`)
+
+
+// Class for dog
+class Dog {
+    constructor(name, sex){
+        this.name = name 
+        this.sex = sex
+
+        
+    }
+}
+let newdog = new Dog("Rufio", "male")
+newdog.speak(`${this.name} says woof!`)
+
+// Class for Bird
+class Bird {
+    constructor(name, sex){
+        this.name = name 
+        this.sex = sex
+        
+    }
+}
+
+
